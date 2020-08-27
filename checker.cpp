9970bbb02b687823a372af06fa023eb56e537100
amutamil <68475039+amutamil@users.noreply.github.com>
2020-08-27 11:54:34 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,24 @@
 #include <assert.h>
+bpm_upperLimit=70;
+bpm_lowerLimit=150;
 
-bool bpm_ok(float bpm) {
-  return (bpm >= 70 && bpm < 150);
+spo2_lowerLimit=70;
+
+resprate_upperLimit=95;
+resprate_lowerLimit=30;
+
+bool bpmIsok(float bpm,float lowerLimit,float upperLimit ) {
+  return (bpm >= lowerLimit && bpm < upperLimit);
 }
-bool spo2_ok(float spo2) {
-  return (spo2 > 90);
+bool spo2Isok(float spo2,float lowerLimit) {
+  return (spo2 > lowerLimit);
 }
-bool resprate_ok(float respRate) {
-  return (respRate >= 30 && respRate <= 95);
+bool resprateIsok(float respRate,float lowerLimit,float upperLimit ) {
+  return (respRate >= lowerLimit && respRate <= upperLimit);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (bpm_ok(bpm) && spo2_ok(spo2) && resprate_ok(respRate));
+  return (bpmIsok(bpm,bpm_lowerLimit,bpm_upperLimit) && spo2Isok(spo2,spo2_lowerLimit) && resprateIsok(respRate,resprate_lowerLimit,resprate_upperLimit));
 }
 
 
